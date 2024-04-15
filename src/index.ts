ef98645ff8584a70c3dbb0a65152452f87667e07
jingming295 <jingming295@gmail.com>
2024-04-15 20:16:17 +0800

    u

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ class init{
     }
     async init(){
         
+        
         // const bs = new BiliBiliSearchApi();
         // const searchResult = await bs.getSearchRequestByTypeVideo('a', 1, 10);
         // console.log(searchResult)

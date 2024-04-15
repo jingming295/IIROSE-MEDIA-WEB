1fa8e58db89d9a0c856b959efe9d92e57b45cf9b
jingming295 <jingming295@gmail.com>
2024-04-16 03:22:24 +0800

    u

--- src/Api/NeteaseAPI/Crypto/index.ts
+++ src/Api/NeteaseAPI/Crypto/index.ts
@@ -11,10 +11,10 @@ export interface EapiResult
     params: string;
 }
 let maxDigits, ZERO_ARRAY, bigZero: BigInt, bigOne: BigInt, dpl10, lr10;
-let highBitMasks = new Array(0, 32768, 49152, 57344, 61440, 63488, 64512, 65024, 65280, 65408, 65472, 65504, 65520, 65528, 65532, 65534, 65535);
-let hexToChar = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f");
-let lowBitMasks = new Array(0, 1, 3, 7, 15, 31, 63, 127, 255, 511, 1023, 2047, 4095, 8191, 16383, 32767, 65535);
-let hexatrigesimalToChar = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+const highBitMasks = new Array(0, 32768, 49152, 57344, 61440, 63488, 64512, 65024, 65280, 65408, 65472, 65504, 65520, 65528, 65532, 65534, 65535);
+const hexToChar = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f");
+const lowBitMasks = new Array(0, 1, 3, 7, 15, 31, 63, 127, 255, 511, 1023, 2047, 4095, 8191, 16383, 32767, 65535);
+const hexatrigesimalToChar = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
 const biRadixBase = 2, biRadixBits = 16, bitsPerDigit = biRadixBits, biRadix = 65536, biHalfRadix = biRadix >>> 1, biRadixSquared = biRadix * biRadix, maxDigitVal = biRadix - 1, maxInteger = 9999999999999998;
 const iv = CryptoJS.enc.Utf8.parse('0102030405060708');
 const presetKey = CryptoJS.enc.Utf8.parse('0CoJUm6Qyw8W8jud');
@@ -68,8 +68,8 @@ function c(a: string, b: string, c: string)
  */
 export function d(param: object, e: string = '010001', f: string = "00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7", g: string = "0CoJUm6Qyw8W8jud")
 {
-    let d = JSON.stringify(param);
-    let h = {
+    const d = JSON.stringify(param);
+    const h = {
         encText: '',
         encSecKey: ''
     }
